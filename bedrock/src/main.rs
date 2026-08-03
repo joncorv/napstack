dd59e96d09fcdf29e36f106ba8f8620ead45e5a5
joncorv <joncorv@gmail.com>
2026-08-03 16:36:33 -0700

    feat: bedrock api wip

--- bedrock/src/main.rs
+++ bedrock/src/main.rs
@@ -1,3 +1,68 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router, extract::State, http::StatusCode, response::IntoResponse, routing::get};
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let shared_state = AppState {
+        population: Arc::new(Mutex::new(500)),
+    };
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/add", get(add))
+        .route("/sub", get(sub))
+        .with_state(shared_state);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Debug, Clone)]
+struct AppState {
+    population: Arc<Mutex<i32>>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct PopulationAnnouncement {
+    population: i32,
+    announcement: String,
+}
+
+#[axum::debug_handler]
+async fn root(State(state): State<AppState>) -> impl IntoResponse {
+    let data = state.population.lock().expect("mutex was poisoned");
+
+    (StatusCode::OK, Json(*data))
+}
+
+#[axum::debug_handler]
+async fn add(State(state): State<AppState>) -> impl IntoResponse {
+    let mut data = state.population.lock().expect("mutex was poisoned");
+    *data += 1;
+
+    let popreturn = PopulationAnnouncement {
+        population: *data,
+        announcement: "Little timmy was born today.".to_string(),
+    };
+
+    (StatusCode::OK, Json(popreturn))
+}
+
+#[axum::debug_handler]
+async fn sub(State(state): State<AppState>) -> impl IntoResponse {
+    let mut data = state.population.lock().expect("mutex was poisoned");
+    *data -= 1;
+
+    let popreturn = PopulationAnnouncement {
+        population: *data,
+        announcement: "Little timmy died in the well.".to_string(),
+    };
+
+    (StatusCode::OK, Json(popreturn))
 }
